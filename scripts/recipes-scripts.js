38465a609b461c2bbd0a03028bfebc9af090c327
Ivan Roev <roevov2@gmail.com>
2024-03-05 14:51:15 +0200

    sorting done

--- scripts/recipes-scripts.js
+++ scripts/recipes-scripts.js
@@ -1,5 +1,4 @@
 const recipes = [
-
     {
         recipeID: 1,
         recipeName: "Miracle no-knead bread",
@@ -10,19 +9,31 @@ const recipes = [
                     It uses no special ingredients, equipment or techniques...`,
         ingredients: ["flour", "water", "yeast", "salt"],
         userID: 1,
-        publicationDate: "Feb 16, 2024",
+        publicationDate: "2025-02-16",
         commentCount: 3,
         likeCount: 6
+    },
+    {
+        recipeID: 2,
+        recipeName: "Edible Cookie Dough",
+        recipeImage: "assets/images/recipe-2-image.webp",
+        recipeText: `This edible cookie dough recipe is egg-free and 
+                    will satisfy any cravings for chocolate chip cookies without the wait.  
+                    I have made this with friends before and they said that they loved the taste....`,
+        ingredients: ["flour", "water", "yeast", "salt"],
+        userID: 1,
+        publicationDate: "2024-02-18",
+        commentCount: 7,
+        likeCount: 16
     }
 ];
 
 const recipesSection = document.querySelector("#recipes-section");
-
-function renderRecipes() {
+function renderRecipes(recipes) {
     for (let recipe of recipes) {
         let recipeCard = `
         <div class="two-column-card recipe-card">
-            <img src="${recipe.recipeImage}" alt="bread image">
+            <img src="${recipe.recipeImage}" alt="">
 
             <div class="text-column">
               <div class="user-info">
@@ -51,4 +62,27 @@ function renderRecipes() {
 
 }
 
-renderRecipes();
+function sortByName(recipes) {
+    return recipes.toSorted();
+}
+
+function sortByDate(recipes) {
+    return recipes.toSorted((a, b) => new Date(b.publicationDate) - new Date(a.publicationDate));
+}
+
+let sortedRecipes = sortByName(recipes).toReversed();
+renderRecipes(sortedRecipes);
+const sortingSelect = document.getElementById("sorting-select");
+
+sortingSelect.addEventListener("change", () => {
+    if (sortingSelect.value === "AZ") {
+        sortedRecipes = sortByName(recipes).toReversed();
+    } else if (sortingSelect.value === "ZA") {
+        sortedRecipes = sortByName(recipes);
+    } else if (sortingSelect.value === "Date") {
+        sortedRecipes = sortByDate(recipes);
+    }
+    recipesSection.innerHTML = "";
+    renderRecipes(sortedRecipes);
+
+});
