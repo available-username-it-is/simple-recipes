0ec8dbd878d65d0e168dbc091964b76c8bf6bbdf
Ivan Roev <roevov2@gmail.com>
2024-03-21 17:58:17 +0200

    deletion of ingredient

--- scripts/submit-scripts.js
+++ scripts/submit-scripts.js
@@ -5,26 +5,36 @@ addIngredientButton.addEventListener("click", () => {
     const ingredientInput = document.getElementById("ingredient-input");
     const amountInput = document.getElementById("amount-input");
     const measurementInput = document.getElementById("measurement-input");
-
-    const ingredient = {
-        ingredient: ingredientInput.value,
-        amount: amountInput.value,
-        measurement: measurementInput.value
-    };
-    ingredients.push(ingredient);
     
+    if (ingredientInput.value && amountInput.value) {
+        const ingredient = {
+            ingredient: ingredientInput.value,
+            amount: amountInput.value,
+            measurement: measurementInput.value
+        };
+        ingredients.push(ingredient);
+        
+        const ingredientLine = document.createElement("li");
+        ingredientLine.innerHTML = `
+            <span>${ingredient.ingredient}</span>
+            <span><b>${ingredient.amount} ${ingredient.measurement}</b></span>
+            <span class="material-symbols-outlined close-icon">
+                close
+            </span>
+        `;
+        ingredientsList.appendChild(ingredientLine); 
     
-    const ingredientLine = document.createElement("li");
-    ingredientLine.innerHTML = `
-        <span><b>${ingredient.amount} ${ingredient.measurement}</b></span>
-        <span>${ingredient.ingredient}</span>
-        <span class="material-symbols-outlined">
-            close
-        </span>
-    `
-    ingredientsList.appendChild(ingredientLine); 
+        ingredientInput.value = "";
+        amountInput.value = "";
+        measurementInput.value = "";
 
-    ingredientInput.value = "";
-    amountInput.value = "";
-    measurementInput.value = "";
+        const closeIcon = ingredientLine.querySelector(".close-icon");
+        closeIcon.addEventListener("click", () => {
+            const index = ingredients.indexOf(ingredient);
+            if (index !== -1) {
+                ingredients.splice(index, 1);
+            }
+            ingredientLine.remove();
+        });
+    }
 })
